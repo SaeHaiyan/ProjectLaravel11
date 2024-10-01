bcdc59e6e549f1545db803fe1d07ed788f9e13ca
SaeHaiyan <hazrinhaiyan987@gmail.com>
2024-10-01 13:37:04 +0700

    update for Model Factories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,17 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Post extends Model
-{
-    use HasFactory;
-}
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'author', 'body'];
+    use HasFactory;
 
 }
 
