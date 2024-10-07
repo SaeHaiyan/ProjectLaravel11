595c923c54a7e157f62493abdeb3e9f4182cf898
SaeHaiyan <hazrinhaiyan987@gmail.com>
2024-10-07 16:24:37 +0700

    Update N + 1 Problem

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function(){
-    return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
+
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function(Post $post){
@@ -25,10 +29,14 @@ Route::get('/posts/{post:slug}', function(Post $post){
 
 Route::get('/authors/{user:username}', function(User $user){
 
+    // $posts = $user->posts->load('category', 'author');
+
     return view('posts', ['title' => count($user->posts) . ' Articles by ' .  $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function(Category $category){
+    
+    // $posts = $category->posts->load('category', 'author');
 
     return view('posts', ['title' => 'Category: ' .  $category->name, 'posts' => $category->posts]);
 });
